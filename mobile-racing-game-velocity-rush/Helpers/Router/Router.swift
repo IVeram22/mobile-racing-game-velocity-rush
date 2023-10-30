080f36882bea6dce0e5175631bc89668a2b7ae9c
Ivan Veramyou <ivan.veramyou22@gmail.com>
2023-10-30 16:05:14 +0100

    Added animation of transitions between screens

--- mobile-racing-game-velocity-rush/Helpers/Router/Router.swift
+++ mobile-racing-game-velocity-rush/Helpers/Router/Router.swift
@@ -7,42 +7,53 @@
 
 import UIKit
 
+private enum Constants {
+    static let duration: CFTimeInterval = 0.3
+}
+
 final class Router {
     static let shared = Router()
     
     private init() {}
     
-    private func open(from viewController: UIViewController, to nextViewController: UIViewController) {
+    private func open(from viewController: UIViewController, to nextViewController: UIViewController, type: CATransitionType, subtype: CATransitionSubtype) {
         nextViewController.modalPresentationStyle = .fullScreen
+        
+        let transition = CATransition()
+        transition.duration = Constants.duration
+        transition.type = type
+        transition.subtype = subtype
+        viewController.view.window?.layer.add(transition, forKey: kCATransition)
+        
         viewController.present(nextViewController, animated: false)
     }
 }
 
 extension Router: StartMenuRouter {
     func startPlaying(from viewController: UIViewController) {
-        open(from: viewController, to: RaceViewController())
+        open(from: viewController, to: RaceViewController(), type: .moveIn, subtype: .fromRight)
     }
     
     func openSettings(from viewController: UIViewController) {
-        open(from: viewController, to: SettingsViewController())
+        open(from: viewController, to: SettingsViewController(), type: .moveIn, subtype: .fromRight)
     }
     
     func seeRecords(from viewController: UIViewController) {
-        open(from: viewController, to: RecordsViewController())
+        open(from: viewController, to: RecordsViewController(), type: .moveIn, subtype: .fromRight)
     }
     
 }
 
 extension Router: GameOverRouter {
     func restartGame(from viewController: UIViewController) {
-        open(from: viewController, to: RaceViewController())
+        open(from: viewController, to: RaceViewController(), type: .moveIn, subtype: .fromBottom)
     }
     
 }
 
 extension Router: BackRouter {
     func comeBack(from viewController: UIViewController) {
-        open(from: viewController, to: ViewController())
+        open(from: viewController, to: ViewController(), type: .reveal, subtype: .fromLeft)
     }
     
 }
